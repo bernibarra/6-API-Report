5dce9313c8186ae851abb26a2fde8768526cdf75
Bernabe Ibarra <bibarra@uw.edu>
2019-02-23 20:17:37 -0800

    finished
    1 late day used

--- civic-info.R
+++ civic-info.R
@@ -1,5 +1,5 @@
 # title: a6 API Report: Congressional Representatives
-# subtitle: "Civic-Info Script"
+# subtitle: "Civic_Info Script"
 # author: "Bernabe Ibarra"
 # date: 20190219
 
@@ -12,14 +12,14 @@ library("tidyr")
 
 source("api-keys.R")
 
-base_uri <- "https://www.googleapis.com/civicinfo/v2"
-endpoint <- "/representatives"
+base_uri <- "https://www.googleapis.com/civicinfo/v2/"
+endpoint <- "representatives"
 address_in <- "2681 NE University Village St Seattle, WA"
 query_params <- list(key = api_key_GCI, address = address_in)
 response <- GET(paste0(base_uri, endpoint), query = query_params)
 
 response_text <- content(response, "text")
-civics_info = fromJSON(response_text)
+civics_info <- fromJSON(response_text)
 
 # Data wrangling
 offices <- civics_info$offices
@@ -27,11 +27,12 @@ officials <- civics_info$official
 
 num_to_rep <- unlist(lapply(civics_info$offices$officialIndices, length))
 
-expanded <- offices[rep(row.names(offices), num_to_rep),]
+expanded <- offices[rep(row.names(offices), num_to_rep), ]
 
-officials <- officials %>% mutate(index = row_number() -1)
+officials <- officials %>% mutate(index = row_number() - 1)
 
-expanded <- expanded %>% mutate(index = row_number() -1) %>%
+expanded <- expanded %>%
+  mutate(index = row_number() - 1) %>%
   rename(position = name)
 
 # Then, join officials and offices by index....
@@ -39,6 +40,7 @@ officials_expanded <- full_join(officials, expanded, by = c("index" = "index"))
 
 rep_df <- officials_expanded %>%
   mutate(emails = replace(emails, emails == "NULL", "Not Available")) %>%
+  mutate(photoUrl = replace_na(photoUrl, "imgs/pixabay_open.png")) %>%
+  mutate(photoUrl = paste0("![", name, "](", photoUrl, ")")) %>%
+  mutate(name = paste0("[", name, "](", urls, ")")) %>%
   select(name, position, party, emails, phones, photoUrl)
-
-# House of Representatives
--- propublica.R
+++ propublica.R
@@ -4,61 +4,69 @@
 # date: 20190219
 
 library("dplyr")
-library("rmarkdown")
 library("httr")
 library("jsonlite")
 library("ggplot2")
 library("tidyr")
+library("stringr")
+library("lubridate")
 
 source("api-keys.R")
 
 query_params <- list(chamber = "house", state_code = "WA")
 base_uri <- "https://api.propublica.org/congress/v1/members/"
-full_address <- paste0(base_uri, query_params$chamber,"/", 
-                       query_params$state_code, "/current.json")
+full_address <- paste0(
+  base_uri, query_params$chamber, "/",
+  query_params$state_code, "/", "current.json"
+)
 
 # returns API response
-get_response <- function(address){
-response <- GET(address, add_headers("X-API-Key" = propublica_key))
-response_text <- content(response, "text")
-propublica_info = fromJSON(response_text)
+get_response <- function(address) {
+  response <- GET(address, add_headers("X-API-Key" = propublica_key))
+  response_text <- content(response, "text")
+  propublica_info <- fromJSON(response_text)
 }
 
+propublica_info <- get_response(full_address)
+
 # Data wrangling
-is.data.frame(propublica_info$results)
-
-
 # Then, join officials and offices by index....
 
 reps_by_gender <- propublica_info$results %>%
-  group_by(gender) %>%
+  mutate(gender = replace(gender, gender == "F", "Females")) %>%
+  mutate(gender = replace(gender, gender == "M", "Males")) %>%
   ggplot(aes(x = gender)) + geom_bar() +
-  labs(title = "Representatives by Gender", y = "# of Representatives")
+  labs(title = "Representatives by Gender",
+       y = "# of Representatives", x = NULL)
 
 reps_by_party <- propublica_info$results %>%
   mutate(party = replace(party, party == "D", "Democrat")) %>%
-  mutate(party = replace(party, party == "R", "Republican")) %>% 
+  mutate(party = replace(party, party == "R", "Republican")) %>%
   ggplot(aes(x = party)) + geom_bar() +
   labs(title = "Representatives by Party", y = "# of Representatives", x = NULL)
 
-member_params <- propublica_info$results[[1,1]]
+member_params <- propublica_info$results[[1, 1]]
 address_members <- paste0(base_uri, member_params, ".json")
-address_votes <- paste0(base_uri, member_params, "/votes.json")
+address_votes <- paste0(base_uri, member_params, "/", "votes.json")
 
+# Member specifcs
 response_members <- get_response(address_members)
 response_votes <- get_response(address_votes)
 
-member_full_df <- full_join(response_members$results, response_votes$results, by = c("member_id" = "member_id"))
+member_full_df <- full_join(response_members$results, response_votes$results, by
+                            = c("member_id" = "member_id"))
 
-str(member_full_df)
-View(member_full_df)
+member_name <- member_full_df %>%
+  select(first_name, middle_name, last_name) %>%
+  mutate(name = paste(first_name, middle_name, last_name, sep = " ")) %>%
+  pull(name)
 
-member_age <- member_full_df$date_of_birth
-member_twitter <- member_full_df$twitter_account
-votes_df <- member_full_df[[1, 4]]
+member_age <- round(time_length( (today() - ymd(member_full_df$date_of_birth)),
+                                unit = "year"), 0)
 
-is.data.frame(votes_df)
-str(votes_list)
-View(member_votes[1])
-     is.list(member_votes)
+member_twitter <- paste0("https://twitter.com/", member_full_df$twitter_account)
 
+votes_df <- member_full_df$votes[[1]]
+vote_aggreement <- round( ( (sum(str_count(votes_df$position, pattern = "Yes"))
+  / (sum(str_count(votes_df$result, pattern = "Passed")) +
+    sum(str_count(votes_df$result, pattern = "Agreed to")))) * 100), 1)
